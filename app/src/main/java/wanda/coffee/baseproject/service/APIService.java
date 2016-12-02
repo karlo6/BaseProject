ab332dddac817ada2ab4a167b69238f9e62acff6
Your Name <you@example.com>
2016-12-02 14:51:49 +0800

    UP: Fixed starting

---
+++ app/src/main/java/wanda/coffee/baseproject/service/APIService.java
@@ -0,0 +1,12 @@
+package wanda.coffee.baseproject.service;
+// Created by jsonjuliane on 12/2/2016.
+
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface APIService {
+    
+
+
+}
